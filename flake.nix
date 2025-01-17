c670d303ef0a44ac58dd97904f12f10ee66cf9d5
Dr3amDin0 <github@dr3amdin0.com>
2025-01-17 14:29:37 +0100

    removed double entry

--- flake.nix
+++ flake.nix
@@ -13,11 +13,6 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     outputs = inputs:
     inputs.snowfall-lib.mkFlake {
         inherit inputs;
