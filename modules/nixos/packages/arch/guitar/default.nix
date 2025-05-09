8a0b21f4af2a1855c9f897074b79a784cf23ee74
Dr3amDin0 <github@dr3amdin0.com>
2025-05-09 11:56:59 +0200

    changed audio settings

--- modules/nixos/packages/arch/guitar/default.nix
+++ modules/nixos/packages/arch/guitar/default.nix
@@ -15,6 +15,19 @@ in
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs;[
       guitarix
+      qpwgraph
     ];
+
+    security.pam.loginLimits = [
+      { domain = "@audio"; item = "memlock"; type = "-"   ; value = "unlimited"; }
+      { domain = "@audio"; item = "rtprio" ; type = "-"   ; value = "99"       ; }
+      { domain = "@audio"; item = "nofile" ; type = "soft"; value = "99999"    ; }
+      { domain = "@audio"; item = "nofile" ; type = "hard"; value = "99999"    ; }
+    ];
+
+    services.udev.extraRules = ''
+      KERNEL=="rtc0", GROUP="audio"
+      KERNEL=="hpet", GROUP="audio"
+    '';
   };
 }
