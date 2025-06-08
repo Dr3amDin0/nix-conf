f3d41a98362c5dc57d022d51dbcc75fe11490095
Dr3amDin0 <github@dr3amdin0.com>
2025-06-08 12:30:40 +0200

    test

--- modules/nixos/packages/arch/core/default.nix
+++ modules/nixos/packages/arch/core/default.nix
@@ -28,7 +28,7 @@ in
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
-
+	#test
     environment.systemPackages = with pkgs;[
       openssh
       gamemode
